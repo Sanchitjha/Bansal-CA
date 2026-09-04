167d5b39a2bd9ec7e63a868864884e007fdcb2fa
Sanchitjha <sanchitjha8888@gmail.com>
2026-09-04 22:50:41 +0530

    feat: implement user module types, route definitions, and admin seeding script

--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -1,21 +1,32 @@
 import { Router } from "express";
 import { IRoute } from "../../common/interfaces/route.interface";
+import { Authenticate } from "../../common/middlewares/auth.middleware";
+import { authorize } from "../../common/middlewares/authorize.middleware";
 import { UserController } from "./user.controller";
 
 export class UserRoute implements IRoute {
   public path = "/api/users";
   public router = Router();
 
-  constructor(private readonly controller: UserController) {
+  constructor(
+    private readonly controller: UserController,
+    private readonly authenticate: Authenticate
+  ) {
     this.initializeRoutes();
   }
 
   private initializeRoutes(): void {
-    this.router.get("/", this.controller.getUsers);
-    this.router.get("/external/:externalId", this.controller.getUserByExternalId);
-    this.router.get("/:id", this.controller.getUserById);
-    this.router.post("/", this.controller.createUser);
-    this.router.put("/:id", this.controller.updateUser);
-    this.router.delete("/:id", this.controller.deleteUser);
+    this.router.use(this.authenticate.handle);
+
+    this.router.get("/", authorize("user.read"), this.controller.getUsers);
+    this.router.get(
+      "/external/:externalId",
+      authorize("user.read"),
+      this.controller.getUserByExternalId
+    );
+    this.router.get("/:id", authorize("user.read"), this.controller.getUserById);
+    this.router.post("/", authorize("user.write"), this.controller.createUser);
+    this.router.put("/:id", authorize("user.write"), this.controller.updateUser);
+    this.router.delete("/:id", authorize("user.write"), this.controller.deleteUser);
   }
 }
