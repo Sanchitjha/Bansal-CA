3c66d930018686d83730d2f3b8223459cffc90bc
Sanchitjha <sanchitjha8888@gmail.com>
2026-09-07 22:48:37 +0530

    feat: implement partner module CRUD operations and service schema management logic

--- server/src/modules/service/service.service.ts
+++ server/src/modules/service/service.service.ts
@@ -2,10 +2,39 @@ import { Types } from "mongoose";
 import { BadRequestException, NotFoundException } from "../../common/errors/http-exception";
 import { ServiceRepository } from "./service.repository";
 import { IService, IServiceWithId, IServicePricing, IServiceRevenueRule } from "./service.types";
+import { FormSchemaModel, IFormSchema } from "./form-schema.model";
+import { ServiceVersionModel, IServiceVersion } from "./service-version.model";
 
 export class ServiceService {
   constructor(private readonly serviceRepository: ServiceRepository) {}
 
+  // Form Schema CRUD
+  public async createFormSchema(data: Partial<IFormSchema>): Promise<IFormSchema> {
+    if (!data.serviceId || !data.title) {
+      throw new BadRequestException("serviceId and title are required for form schema");
+    }
+    return FormSchemaModel.create(data);
+  }
+
+  public async getFormSchemas(serviceId?: string): Promise<IFormSchema[]> {
+    const query: any = {};
+    if (serviceId) query.serviceId = serviceId;
+    return FormSchemaModel.find(query).sort({ version: -1 }).lean() as unknown as IFormSchema[];
+  }
+
+  // Service Versions CRUD
+  public async createServiceVersion(data: Partial<IServiceVersion>): Promise<IServiceVersion> {
+    if (!data.serviceId || !data.versionNumber) {
+      throw new BadRequestException("serviceId and versionNumber are required for service version");
+    }
+    return ServiceVersionModel.create(data);
+  }
+
+  public async getServiceVersions(serviceId: string): Promise<IServiceVersion[]> {
+    return ServiceVersionModel.find({ serviceId }).sort({ versionNumber: -1 }).lean() as unknown as IServiceVersion[];
+  }
+
+  // Core Service CRUD
   public async getServices(): Promise<IServiceWithId[]> {
     return this.serviceRepository.findAll();
   }
